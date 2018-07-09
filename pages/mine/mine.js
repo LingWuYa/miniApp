06cc88cb3ba593f94f2e1c15be5be286c1a60707
LingWuYa <478399567@qq.com>
2018-07-10 00:55:52 +0800

    获取用户信息

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,20 +1,37 @@
 // pages/mine/mine.js
 Page({
-
+  
   /**
    * 页面的初始数据
    */
   data: {
-  
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              console.log(res.userInfo)
+            }
+          })
+        }
+      }
+    })
   },
-
+  bindGetUserInfo: function (e) {
+    this.setData({
+      userInfo: e.detail.userInfo
+    })
+     wx.setStorageSync("userInfo", e.detail.userInfo)
+    // console.log(e.detail.userInfo)
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
